c48d47c1cbe4bf9eab9a7f6e428f21d87994c66e
Tin Tran <tin.tran0519@gmail.com>
2017-03-05 22:01:49 -0800

    post service added

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 var app = angular.module("reddit_clone", []);
 
-app.controller("MainCtrl", MainCtrl);
+app.controller("MainCtrl", MainCtrl)
+   .factory('postService',postService);
 
 MainCtrl.inject = ['$scope']
 
-function MainCtrl($scope){
-  $scope.test = "I'm working!!";
-  $scope.posts = [
+// service controller
+function postService(){
+  var postService = {
+    posts: [
     {title: 'post 1', upvotes: 5},
     {title: 'post 2', upvotes: 2},
     {title: 'post 3', upvotes: 15},
     {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+    {title: 'post 5', upvotes: 4},
+    {title: 'new guy', upvotes: 19}
+    ]
+  };
+  return postService;
+}
+
+function MainCtrl($scope,postService){
+  $scope.test = "I'm working!!";
+  $scope.posts = postService.posts
   $scope.addPost = addPost;
   $scope.incrementUpvotes = incrementUpvotes;
 
