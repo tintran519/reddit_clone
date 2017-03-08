1b8d8170b7c1432c406734745efd31de1fd8436e
Tin Tran <tin.tran0519@gmail.com>
2017-03-07 22:41:25 -0800

    get,show,post routes for Posts added

---
+++ server/controllers/post.js
@@ -0,0 +1,44 @@
+var Post = require("../models/Posts");
+
+var index = function(req, res, next){
+  Post.find({}, function(err, posts){
+    if(err) return next(err);
+
+    // return posts
+    res.json(posts);
+  })
+}
+
+var create = function(req, res, next){
+  // create new instance of post with settings from body
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err) return next(err);
+
+    res.json(post);
+  })
+}
+
+var preload = function(req, res, next, id){
+  var query = Post.findById(id);
+
+  query.exec(function(err, post){
+    if(err) return next(err);
+    if(!post) return next(new Error('can\'t find post'));
+
+    req.post = post;
+    return next();
+  })
+}
+
+var show = function(req, res){
+  res.json(req.post);
+}
+
+module.exports = {
+  index: index,
+  create: create,
+  preload: preload,
+  show: show
+}
---
+++ server/routes/routes.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = new express.Router();
+
+// Require controllers
+var PostCtrl = require('../controllers/post');
+// router.get('/',function(req,res,next){res.render('index')})
+
+// Posts routes
+router.get('/posts', PostCtrl.index);
+router.post('/posts', PostCtrl.create);
+router.param('post', PostCtrl.preload);
+router.get('/posts/:post', PostCtrl.show);
+
+module.exports = router;
