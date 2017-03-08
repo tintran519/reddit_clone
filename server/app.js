beb7419efdeb2cef2e8398e75cf40e8ed3a2824b
Tin Tran <tin.tran0519@gmail.com>
2017-03-07 21:29:20 -0800

    Post model added

--- server/app.js
+++ server/app.js
@@ -10,6 +10,12 @@ var users = require('./routes/users');
 
 var app = express();
 
+// Connect to MongoDB
+var mongoose = require('mongoose');
+require('./models/Posts');
+
+mongoose.connect('mongodb://localhost/news')
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
