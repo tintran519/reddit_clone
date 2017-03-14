1e87f8ce110d5571e2a8b2cb4d53e0b5816c5b7c
Tin Tran <tin.tran0519@gmail.com>
2017-03-13 22:07:11 -0700

    set and validate password methods created

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
 
 // ===============================
 // User Schema
@@ -8,6 +9,20 @@ var UserSchema = new mongoose.Schema({
   hash: String,
   salt: String
 });
+
+// Accepts pw then generates salt associated with pw hash
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+// Accepts pw and compares it to the hash stored, returning boolean
+UserSchema.methods.validPassword = function(password){
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+  return this.hash === hash;
+}
 
 var User = mongoose.model('User', UserSchema);
 
